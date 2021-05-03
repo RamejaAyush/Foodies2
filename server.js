2d7984b6b8825277c01061b2d5d99b4e2093ab08
Ayush Rameja <ayushrwork@gmail.com>
2021-05-03 19:45:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+// ejs is the view engine now
+app.set('view engine', 'ejs')
+
+// middleware
+app.use(express.static('public'))
+
+// port
+app.listen(3000)
+
+// routes
+app.get('/', (req, res) => {
+  res.render('index', { title: 'home' })
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'contact' })
+})
+
+app.get('/cooking', (req, res) => {
+  res.render('cooking', { title: 'cooking' })
+})
+
+app.get('/order', (req, res) => {
+  res.render('order', { title: 'order' })
+})
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { title: '404' })
+})
