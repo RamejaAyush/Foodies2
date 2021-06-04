1dceefc0a2614105097a2e1c2e0795788a903cdb
Ayush Rameja <ayushrwork@gmail.com>
2021-06-04 12:50:05 +0530

    added mongodb auth but very buggy fixing on a way

--- server.js
+++ server.js
@@ -1,52 +1,67 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
-// ejs is the view engine now
-app.set('view engine', 'ejs')
+const app = express();
 
 // middleware
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
 
-// port
-app.listen(3000)
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI =
+ 'mongodb+srv://ayush:test@cluster0.xfnhm.mongodb.net/node-auth?retryWrites=true&w=majority';
+mongoose
+ .connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+ })
+ .then((result) => app.listen(3000))
+ .catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => {
-  res.render('index', { title: 'home' })
-})
+app.get('*', checkUser);
+app.use(authRoutes);
 
-app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'contact' })
-})
+app.get('/', (req, res) => res.render('index', { title: 'home' }));
 
-app.get('/cooking', (req, res) => {
-  res.render('cooking', { title: 'cooking' })
-})
+app.get('/about', (req, res) => res.render('about', { title: 'about' }));
 
-app.get('/order', (req, res) => {
-  res.render('order', { title: 'order' })
-})
+app.get('/contact', requireAuth, (req, res) =>
+ res.render('contact', { title: 'contact' })
+);
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'about' })
-})
+app.get('/cooking', requireAuth, (req, res) =>
+ res.render('cooking', { title: 'cooking' })
+);
 
-app.get('/meals', (req, res) => {
-  res.render('meals', { title: 'meals' })
-})
+app.get('/order', requireAuth, (req, res) =>
+ res.render('order', { title: 'order' })
+);
 
-app.get('/recipe', (req, res) => {
-  res.render('recipe', { title: 'recipe' })
-})
+app.get('/meals', requireAuth, (req, res) =>
+ res.render('meals', { title: 'Meals' })
+);
 
-app.get('/login', (req, res) => {
-  res.render('login', { title: 'login' })
-})
+app.get('/recipe', requireAuth, (req, res) =>
+ res.render('recipe', { title: 'recipe' })
+);
 
-app.get('/register', (req, res) => {
-  res.render('register', { title: 'register' })
-})
+// app.get('/login', (req, res) => {
+//  res.render('login', { title: 'login' });
+// });
+
+// app.get('/register', (req, res) => {
+//  res.render('register', { title: 'register' });
+// });
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { title: '404' })
-})
+ res.status(404).render('404', { title: '404' });
+});
